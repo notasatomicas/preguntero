56c16225f4a26799fa89935f132dfbb9606e5488
Notasatomicas <notasatomicas@gmail.com>
2025-05-13 11:50:03 -0300

    Minor fixes en links de navbar y footer

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,17 +1,26 @@
 <footer class="py-3 my-4">
   <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+
+
     <li class="nav-item">
-      <a href="#" class="nav-link px-2 text-body-secondary">Home</a>
+      <a href="<?= base_url('/') ?>" class="nav-link px-2 text-body-secondary">🌐 Inicio</a>
     </li>
+
     <li class="nav-item">
-      <a href="#" class="nav-link px-2 text-body-secondary">Contacto</a>
+      <a href="<?= base_url('cargar') ?>" class="nav-link px-2 text-body-secondary">🚀 Cargar</a>
     </li>
+
     <li class="nav-item">
-      <a href="#" class="nav-link px-2 text-body-secondary">Acerca de</a>
+      <a href="<?= base_url('contacto') ?>" class="nav-link px-2 text-body-secondary">📞 Contacto</a>
     </li>
+
     <li class="nav-item">
-      <a href="#" class="nav-link px-2 text-body-secondary">Otro</a>
+      <a href="<?= base_url('about') ?>" class="nav-link px-2 text-body-secondary">ℹ️ Acerca de</a>
     </li>
+
+
   </ul>
+
   <p class="text-center text-body-secondary">&copy; <?= date('Y') ?> Preguntero</p>
+
 </footer>
--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -1,34 +1,24 @@
-<nav
-  class="navbar navbar-expand-md navbar-dark bg-dark"
-  aria-label="Fourth navbar example"
->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Preguntero</a>
-
+    <a class="navbar-brand" href="<?= base_url('/') ?>">📜 Preguntero</a>
 
     <button
       class="navbar-toggler"
       type="button"
       data-bs-toggle="collapse"
-      data-bs-target="#navbarsExample04"
-      aria-controls="navbarsExample04"
+      data-bs-target="#navBar"
       aria-expanded="false"
-      aria-label="Toggle navigation"
     >
       <span class="navbar-toggler-icon"></span>
     </button>
 
-
-    <div class="collapse navbar-collapse" id="navbarsExample04">
+    <div class="collapse navbar-collapse" id="navBar">
       <ul class="navbar-nav ms-auto mb-2 mb-md-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-        </li>
-        <li class="nav-item"><a class="nav-link" href="./cargar">Cargar</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">Contacto</a></li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        <li class="nav-item"><a class="nav-link active" aria-current="page" href="<?= base_url('/') ?>">🌐 Inicio</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?= base_url('cargar') ?>">🚀 Cargar</a></li>
+        <!-- <li class="nav-item"><a class="nav-link" href="<?= base_url('contacto') ?>">📞 Contacto</a></li>
+
+        <li class="nav-item"><a class="nav-link disabled" aria-disabled="true">Disabled</a></li> -->
       </ul>
     </div>
   </div>
